f56c0d92e28f6c68e285689f11cc9ceba8734432
ricardO <sleiph@gmail.com>
2024-08-20 16:07:57 -0300

    criando inputs das cartas

---
+++ components/Carta.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    const props = defineProps(['nome', 'checada']);
+
+    //
+</script>
+
+<template>
+    <li>
+        <input type="radio" :id="props['nome']" name="hosting" :value="props['nome']" class="hidden peer" :checked="props['checada']"  />
+        <label :for="props['nome']" class="inline-flex justify-center w-full p-5 border border-cinza-claro rounded-lg cursor-pointer peer-checked:border-rosa peer-checked:text-rosa peer-checked:bg-cinza-escuro hover:bg-cinza bg-cinza-claro hover:bg-cinza-escuro">                           
+            <div>{{props['nome']}}</div>
+        </label>
+    </li>
+</template>
--- pages/salas/[hash].vue
+++ pages/salas/[hash].vue
@@ -10,15 +10,15 @@
     <div class="w-full col-span-10 col-start-2 py-4 px-8 text-center bg-cinza border-cinza-claro rounded-lg shadow">
         
     </div>
-    <div class="w-full grid grid-cols-7 gap-4 col-span-10 col-start-2 py-4 px-8 text-center bg-cinza border-cinza-claro rounded-lg shadow">
-        <div></div>
-        <div class="p-4 w-24 text-center bg-cinza-escuro border-cinza rounded-lg shadow">1</div>
-        <div class="p-4 w-24 text-center bg-cinza-escuro border-cinza rounded-lg shadow">2</div>
-        <div class="p-4 w-24 text-center bg-cinza-escuro border-cinza rounded-lg shadow">3</div>
-        <div class="p-4 w-24 text-center bg-cinza-escuro border-cinza rounded-lg shadow">5</div>
-        <div class="p-4 w-24 text-center bg-cinza-escuro border-cinza rounded-lg shadow">?</div>
-        <div></div>
-    </div>
+    <ul class="w-full grid grid-cols-7 gap-4 col-span-10 col-start-2 py-4 px-8 text-center bg-cinza border-cinza-claro rounded-lg shadow">
+        <li></li>
+        <Carta nome="1" />
+        <Carta nome="2" />
+        <Carta nome="3" />
+        <Carta nome="5" />
+        <Carta nome="?" checada="true" />
+        <li></li>
+    </ul>
     <div class="col-start-11 row-start-3 justify-self-end">
         <NuxtLink to="/">
             <Icon name="bx:bxs-home" class="text-xl"/>
